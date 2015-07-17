43cd3599496c9727640071744e176abc2cf0f6a6
Roland <rolkwo@gmail.com>
2015-07-17 14:19:13 +0200

    Fixed vin conversion

--- VechicleInfoHandler.cpp
+++ VechicleInfoHandler.cpp
@@ -1,5 +1,6 @@
 #include "VechicleInfoHandler.h"
 
+#include <algorithm>
 #include <sstream>
 #include <vector>
 
@@ -17,17 +18,24 @@ std::string VechicleInfoHandler::processLines(const std::vector<std::string>& li
     for(const auto& line : lines)
         oneLine += line;
 
+    auto newEnd = std::remove(oneLine.begin(), oneLine.end(), ' ');
+    oneLine.erase(newEnd, oneLine.end());
+
     std::string vin;
     while(!oneLine.empty())
     {
         std::string hex = oneLine.substr(0, 2);
-        std::stringstream ss(hex);
+        std::stringstream ss;
 
-        char c;
-        ss >> std::hex >> c;
+        ss << std::hex << hex;
 
-        vin += c;
-        oneLine.erase(0, 3);
+        unsigned int c;
+        ss >> c;
+
+        oneLine.erase(0, 2);
+
+        if(c)
+            vin += c;
     }
 
     return vin;
